c106f3c31d9f9e0b90d48fe1ead8717dce471a12
zihuaweng <zihuajw@gmail.com>
2025-11-11 22:22:52 -0800

    Add solution to 1143

--- leetcode_python/1143.Longest_Common__Subsequence.py
+++ leetcode_python/1143.Longest_Common__Subsequence.py
@@ -18,3 +18,31 @@ class Solution:
                 else:
                     dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
         return dp[m][n]
+
+# Reverse recursive
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return self.get_subsequence(text1, text2, len(text1)-1,len(text2)-1)
+    @cache
+    def get_subsequence(self, text1, text2, index1, index2):
+        if index1 == -1 or index2 == -1:
+            return 0
+
+        if text1[index1] == text2[index2]:
+            return self.get_subsequence(text1, text2, index1-1, index2-1) + 1
+        else:
+            return max(self.get_subsequence(text1, text2, index1, index2-1), self.get_subsequence(text1, text2, index1-1, index2))
+
+# Forward recursive
+class Solution:
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        return self.get_subsequence(text1, text2, 0,0)
+    @cache
+    def get_subsequence(self, text1, text2, index1, index2):
+        if index1 == len(text1) or index2 == len(text2):
+            return 0
+
+        if text1[index1] == text2[index2]:
+            return self.get_subsequence(text1, text2, index1+1, index2+1) + 1
+        else:
+            return max(self.get_subsequence(text1, text2, index1, index2+1), self.get_subsequence(text1, text2, index1+1, index2)
